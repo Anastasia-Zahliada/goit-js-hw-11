58da9b2ba6557fa8824d972f3378d26f42596d82
Anastasia-Zahliada <zahliadaa@gmail.com>
2026-02-15 14:13:22 +0100

    11 HW done

---
+++ src/js/render-functions.js
@@ -0,0 +1,51 @@
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+const galleryEl = document.querySelector('.gallery');
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
+
+export function createGallery(images) {
+  const markup = images
+    .map(
+      ({
+        webformatURL,
+        largeImageURL,
+        tags,
+        likes,
+        views,
+        comments,
+        downloads,
+      }) => `
+      <li class="gallery-item">
+        <a class="gallery-link" href="${largeImageURL}">
+          <img class="gallery-image" src="${webformatURL}" alt="${tags}" loading="lazy" />
+        </a>
+        <div class="info">
+          <p class="info-item"><b>Likes</b><span>${likes}</span></p>
+          <p class="info-item"><b>Views</b><span>${views}</span></p>
+          <p class="info-item"><b>Comments</b><span>${comments}</span></p>
+          <p class="info-item"><b>Downloads</b><span>${downloads}</span></p>
+        </div>
+      </li>`
+    )
+    .join('');
+
+  galleryEl.insertAdjacentHTML('beforeend', markup);
+  lightbox.refresh();
+}
+
+export function clearGallery() {
+  galleryEl.innerHTML = '';
+}
+
+export function showLoader() {
+  document.querySelector('.loader')?.classList.remove('is-hidden');
+}
+
+export function hideLoader() {
+  document.querySelector('.loader')?.classList.add('is-hidden');
+}
